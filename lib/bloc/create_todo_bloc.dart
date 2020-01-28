6bfd2d286740d0087a471802279a89b49ff20e39
fummicc1 <fumiya989@gmail.com>
2020-01-28 18:59:58 +0900

    write create_todo_bloc_code

--- lib/bloc/create_todo_bloc.dart
+++ lib/bloc/create_todo_bloc.dart
@@ -2,6 +2,7 @@ import 'dart:async';
 
 import 'package:rxdart/rxdart.dart';
 import 'package:todooo/data/local_storage_client.dart';
+import 'package:todooo/data/todo.dart';
 
 class CreateToDoBloc {
 
@@ -10,7 +11,25 @@ class CreateToDoBloc {
   BehaviorSubject<bool> _isPersistingSubject = BehaviorSubject();
   Stream<bool> get isPersisting => _isPersistingSubject.stream;
 
-  CreateToDoBloc(this._repository);
+  StreamController<void> _saveButtonTapController = StreamController();
+  Sink<void> get saveButtonTapSink => _saveButtonTapController.sink;
 
+  BehaviorSubject<String> _contentTextController = BehaviorSubject();
+  Sink<String> get contentTextStream => _contentTextController.sink;
+  BehaviorSubject<String> _deadlineRadioButtonController = BehaviorSubject();
+  Sink<String> get deadlineRadioButtonSink => _deadlineRadioButtonController.sink;
 
+  CreateToDoBloc(this._repository) {
+    _saveButtonTapController.stream.listen((_) async {
+      _isPersistingSubject.add(true);
+      ToDo toDo = ToDo(_contentTextController.value, _deadlineRadioButtonController.value, DateTime.now());
+      await createToDo(toDo);
+      _isPersistingSubject.add(false);
+    });
+  }
+
+  Future<bool> createToDo(ToDo todo) {
+    if (todo.content.isEmpty || todo.isDone || todo.isOver) return Future.value(false);
+    return _repository.saveToDo(todo);
+  }
 }
--- test/create_todo_bloc_test.dart
+++ test/create_todo_bloc_test.dart
@@ -1,6 +1,25 @@
 import 'package:test/test.dart';
+import 'package:todooo/bloc/create_todo_bloc.dart';
+import 'package:todooo/data/local_storage_client_mock.dart';
+import 'package:todooo/data/todo.dart';
 
 void main() {
-  test("test for todo creation", () {
+
+  CreateToDoBloc createToDoBloc;
+
+  setUp(() {
+    createToDoBloc = CreateToDoBloc(LocalStorageClientMock());
+  });
+
+  test("test for todo creation successfully", () async {
+    ToDo toDo = ToDo("Test", "everyday", DateTime.now());
+    var result = await createToDoBloc.createToDo(toDo);
+    expect(result, true);
+  });
+
+  test('test for todo creation because content is empty.', () async {
+    ToDo toDo = ToDo("", "today", DateTime.now());
+    var result = await createToDoBloc.createToDo(toDo);
+    expect(result, false);
   });
 }
