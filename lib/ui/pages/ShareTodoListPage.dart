373981e04e6c16d169098788a5c8b5261817130d
fummicc1 <fumiya.tennis1@gmail.com>
2021-08-10 21:06:03 +0900

    Start implementation for ShareTodoList feature

---
+++ lib/ui/pages/ShareTodoListPage.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+class ShareTodoListPage extends StatelessWidget {
+  const ShareTodoListPage({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
